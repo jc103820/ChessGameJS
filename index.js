17d87240764162cb53b20879f52797ec3b8af224
jc103820 <jc103820@ohio.edu>
2022-04-29 15:29:37 -0400

    Canvas that places Rectangles (knows where im pointing)

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-window.onload=function() {
-    
+const canvas = document.getElementById('gameBoard');
+const ctx = canvas.getContext('2d');
+
+const xOffset = canvas.offsetLeft;
+const yOffset = canvas.offsetTop;
+
+const drawCircle = (e) => {
+    ctx.beginPath();
+    ctx.rect(e.clientX - xOffset - 10,e.clientY - yOffset - 10,20,20); //Places it centered on the mouse
+    ctx.stroke();
 }
+
+canvas.addEventListener('mousedown', drawCircle);
